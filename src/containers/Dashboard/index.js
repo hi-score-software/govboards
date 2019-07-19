ee0d1ccb028bbfb5b5502faf34b5f6919ce2390a
dansmog <juwonanthony@gmail.com>
2019-07-19 18:30:37 +0100

    Updates

--- src/containers/Dashboard/index.js
+++ src/containers/Dashboard/index.js
@@ -1,20 +1,21 @@
 import React, { PureComponent } from "react";
-import { Switch, Route, Redirect } from "react-router-dom";
+import { Switch, Route, Redirect, NavLink } from "react-router-dom";
 
 import "./dashboard.css";
 import * as LoadableRoutes from "../../routes";
 
 class Dashboard extends PureComponent {
   state = {
-    isMobileMenuVisible: false,
-  }
+    isMobileMenuVisible: false
+  };
 
   showMobileMenu = () => {
     this.setState({
-      isMobileMenuVisible: true,
-    })
-  }
+      isMobileMenuVisible: true
+    });
+  };
   render() {
+    const path = this.props.match.path;
     return (
       <section className="dashboard-wrapper">
         <section className="dashboard-content">
@@ -29,8 +30,113 @@ class Dashboard extends PureComponent {
                         alt="logo"
                       />
                     </div>
-                    <div className="show-on-mobile" onClick={this.showMobileMenu}>
-                      <img src={require("../../assets/Icons/menu.svg")} alt=""/>
+                    <div className="nav_menu-list hide-on-mobile">
+                      <NavLink
+                        exact
+                        to={`${path}/economy`}
+                        activeClassName="active"
+                      >
+                        <img
+                          src={require("../../assets/images/economy.svg")}
+                          alt=""
+                          className="icon-size"
+                        />
+                        <span>Economy</span>
+                      </NavLink>
+                      <NavLink
+                        exact
+                        to={`${path}/labour-business-and-environment`}
+                        activeClassName="active"
+                      >
+                        <img
+                          src={require("../../assets/images/labour.svg")}
+                          alt=""
+                          className="icon-size"
+                        />
+                        <span>Labour Business and environment</span>
+                      </NavLink>
+                      <NavLink
+                        exact
+                        to={`${path}/social-services-and-investments`}
+                        activeClassName="active"
+                      >
+                        <img
+                          src={require("../../assets/images/social-services.svg")}
+                          alt=""
+                          className="icon-size"
+                        />
+                        <span>Social Services & Investments</span>
+                      </NavLink>
+                      <NavLink
+                        exact
+                        to={`${path}/travel-and-immigration`}
+                        activeClassName="active"
+                      >
+                        <img
+                          src={require("../../assets/images/travel-and-immigration.svg")}
+                          alt=""
+                          className="icon-size"
+                        />
+                        <span>Travel & Immigration</span>
+                      </NavLink>
+                      <NavLink
+                        exact
+                        to={`${path}/energy`}
+                        activeClassName="active"
+                      >
+                        <img
+                          src={require("../../assets/images/energy.svg")}
+                          alt=""
+                          className="icon-size"
+                        />
+                        <span>Energy</span>
+                      </NavLink>
+                      <NavLink
+                        exact
+                        to={`${path}/education`}
+                        activeClassName="active"
+                      >
+                        <img
+                          src={require("../../assets/images/education.svg")}
+                          alt=""
+                          className="icon-size"
+                        />
+                        <span>Education</span>
+                      </NavLink>
+                      <NavLink
+                        exact
+                        to={`${path}/agriculture`}
+                        activeClassName="active"
+                      >
+                        <img
+                          src={require("../../assets/images/agriculture.svg")}
+                          alt=""
+                          className="icon-size"
+                        />
+                        <span>Agriculture</span>
+                      </NavLink>
+
+                      <NavLink
+                        exact
+                        to={`${path}/ict`}
+                        activeClassName="active"
+                      >
+                        <img
+                          src={require("../../assets/images/ict.svg")}
+                          alt=""
+                          className="icon-size"
+                        />
+                        <span>ict</span>
+                      </NavLink>
+                    </div>
+                    <div
+                      className="show-on-mobile"
+                      onClick={this.showMobileMenu}
+                    >
+                      <img
+                        src={require("../../assets/Icons/menu.svg")}
+                        alt=""
+                      />
                     </div>
                   </div>
                 </div>
